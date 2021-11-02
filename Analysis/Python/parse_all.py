56c5b3c525ee8af2de2c854222062e5130a4d758
Oliver Lountain <oliverlountain@Olivers-iMac.local>
2021-11-02 10:32:53 +1030

    added file to parse and save all simout files within a given directory in a new set of directories as csv files

---
+++ Analysis/Python/parse_all.py
@@ -0,0 +1,18 @@
+import glob
+import sys
+import os
+
+sys.path.insert(0, 'Analysis/Python')
+from parse_simout import *
+from helper_functions import tic, toc
+
+my_files = glob.glob("simout/pattern_ex/**/*.simout", recursive=True)
+tic()
+for file in my_files:
+    simout_dict = parse_simout(file)
+    _, coords, counts = parse_last_frame(simout_dict, density = False)
+    data = np.hstack((coords, counts.reshape(len(counts),1)))
+    os.makedirs(os.path.dirname("processed_data/" + os.path.splitext(file)[0]), exist_ok=True)
+    np.savetxt("processed_data/" + os.path.splitext(file)[0] + ".csv", data, delimiter=",", header = "x,y,z", comments="")
+
+toc()
